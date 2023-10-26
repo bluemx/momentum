b44a33e93dd3fb5a0630a72bfc2b6833a34ee10d
Albin Rodriguez <ealbinu@gmail.com>
2023-10-25 19:02:46 -0600

    dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://digitalfamily.mx/momentumv1',
   experimental: {
     viewTransitions: true,
   },
